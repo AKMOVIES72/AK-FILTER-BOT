1ee884c7c5fbe8c0dd30c80e47bdecaca7e05742
AKMOVIES72 <akmovies728@gmail.com>
2025-02-08 00:49:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -161,7 +161,7 @@ PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
     ON_HEROKU = True
 else:
-    ON_HEROKU = True
+    ON_HEROKU = False
 URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
 
 
